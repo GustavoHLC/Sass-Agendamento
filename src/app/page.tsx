c11a6d0f3525935fb73ecd6b0e7f7ef212916d50
GustavoHLC <gutohenri_car@hotmail.com>
2025-05-28 00:56:03 -0300

    chore: adicionando setup do shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-const Home = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <h1 className="mb-4 text-4xl font-bold">Home Page</h1>
-    </div>
-  );
-};
+import { Button } from "@/components/ui/button";
 
-export default Home;
+export default function Page() {
+  return <Button>teste!</Button>;
+}
